411fae6ea745731396f317afda43262b5cd7aecd
Charles Zhou <zhou@linux.com>
2023-09-21 13:28:27 -0500

    Add style

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,14 +1,22 @@
-import { Card, CardBody, CardHeader } from '@nextui-org/react';
+import { Card, CardBody, CardHeader, Divider } from '@nextui-org/react';
 
 const Course = ({ course }) => (
-    <Card shadow>
-        <CardHeader>{course.number}: {course.title}</CardHeader>
-        <CardBody>{course.meets}</CardBody>
+    <Card shadow className="basis-1/4 h-48">
+        <CardHeader className="flex gap-3">
+            <div className="flex flex-col">
+                <p className="text-md">{course.term} CS {course.number}</p>
+                <p className="text-small text-default-500">{course.title}</p>
+            </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+            <p>{course.meets}</p>
+        </CardBody>
     </Card>
 );
 
 const CourseList = ({ courses }) => (
-    <div>
+    <div class="grid md:grid-cols-4 gap-4 w-5/6 sm:grid-cols-1 m-4">
         {Object.values(courses).map(course => <Course course={course} />)}
     </div>
 );
